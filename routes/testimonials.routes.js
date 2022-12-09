b98cba13b5727eaf790884cc59d04933f0699fb1
lapivoinerouge <wioladys@gmail.com>
2022-12-09 23:04:37 +0100

    Create basic project structure and testimonial endpoints

---
+++ routes/testimonials.routes.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router()
+const db = require('../db').testimonials;
+
+const generateNextId = () => {
+    return (db[db.length-1]).id + 1;
+}
+
+const getElementById = x => {
+    return db.find(({id}) => id.toString() === x);
+}
+
+router.route('/testimonials').get((req, res) => {
+    res.json(db);
+});
+
+router.route('/testimonials/random').get((req, res) => {
+    if (db.length === 0) {
+        res.status(404).send();
+    }
+    const idx = Math.floor(Math.random() * db.length + 1)
+    res.json(db.find(({ id }) => id === idx));
+});
+
+router.route('/testimonials/:id').get((req, res) => {
+    const entry = getElementById(req.params.id);
+    if (entry) {
+        res.json(entry);
+    } else {
+        res.status(404).json(`Element with id ${req.params.id} not found`)
+    }
+});
+
+router.route('/testimonials').post((req, res) => {
+    const idx = generateNextId();
+    db.push({id: idx, ...req.body });
+
+    res.json(db.find(({ id }) => id === idx));
+});
+
+router.route('/testimonials/:id').put((req, res) => {
+    const entry = getElementById(req.params.id);
+    if (entry) {
+        entry.author = req.body.author;
+        entry.text = req.body.text;
+        res.json(entry);
+    } else {
+        res.status(404).json(`Element with id ${req.params.id} not found`);
+    }
+});
+
+router.route('/testimonials/:id').delete((req, res) => {
+    const entry = getElementById(req.params.id);
+    if (entry) {
+        const idx = db.indexOf(entry);
+        db.splice(idx, 1);
+        res.status(204).json({ message: "No content"});
+    } else {
+        res.status(404).json(`Element with id ${req.params.id} not found`);
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = '8000';
+
+const testimonialRoutes = require('./routes/testimonials.routes');
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', testimonialRoutes);
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+  })
+
+app.listen(port, () => {
+    console.log("Server is running on port " + port);
+});
