59a098206a3967d05d379ad74d807e7b30c40b58
lapivoinerouge <wioladys@gmail.com>
2022-12-11 21:19:57 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = '8000';
 
 const testimonialRoutes = require('./routes/testimonials.routes');
 const concertRoutes = require('./routes/concerts.routes');
 const seatRoutes = require('./routes/seats.routes');
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
@@ -15,10 +19,10 @@ app.use('/api', testimonialRoutes);
 app.use('/api', concertRoutes);
 app.use('/api', seatRoutes);
 
-app.use((req, res) => {
-    res.status(404).json({ message: 'Not found' });
-  })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
-app.listen(port, () => {
-    console.log("Server is running on port " + port);
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
